016f56bc2f434783ac5155d783f08a08b1606f38
ikramawol <ikruawol.14@gmail.com>
2022-04-11 02:56:25 -0700

    prints the name of the file it was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main: prints the name of the file it was compiled from
+ * main - prints the name of the file it was compiled from
  *
- * Return: 0 if successful
+ * Return: Everything worked ok 
  */
  
 int main(void)
 {
 
   printf("%s\n",__BASE_FILE__);
-  printf("\n");
   return (0);
 }
